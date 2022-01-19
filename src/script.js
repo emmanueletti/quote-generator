4b1cbcf7e61db89380fab5a8c25b7d4a338c02cf
Emmanuel Etti <emmanueletti@gmail.com>
2022-01-18 18:11:45 -0800

    adds ability to show loading state

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-// Variables in the global scope
+// Api Quotes state in the global scope
 let apiQuotes = [];
 
 // Pro-tip: cache references to DOM elements in the global scope
@@ -8,14 +8,28 @@ const quoteText = document.querySelector('#quote');
 const authorText = document.querySelector('#author');
 const twitterBtn = document.querySelector('#twitter');
 const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
 
 // Get random int
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+// Show loading
+function showLoading() {
+  // Hidden property is present on any HTML DOM element!!!
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function hideLoading() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // Generate new quote
 function newQuote() {
+  showLoading();
   // Pick random quote from api quotes array
   const quote = apiQuotes[getRandomInt(apiQuotes.length)];
   authorText.textContent = quote.author ?? 'Unknown';
@@ -25,10 +39,13 @@ function newQuote() {
   quote.text.length > 120
     ? quoteText.classList.add('long-quote')
     : quoteText.classList.remove('long-quote');
+
+  hideLoading();
 }
 
 // Fetch quotes from API
 async function getQuotes() {
+  showLoading();
   const API_URL = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(API_URL);
@@ -51,7 +68,7 @@ function tweetQuote() {
 
 // On Load
 getQuotes();
-
+// showLoading();
 // Event listeners
 twitterBtn.addEventListener('click', tweetQuote);
 newQuoteBtn.addEventListener('click', getQuotes);
